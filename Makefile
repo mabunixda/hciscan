9215f4fa0c53f070516c95d7e73742b120eb6b1b
Martin Buchleitner <mabunixda@gmail.com>
2020-09-30 13:21:07 +0000

    refactoring to avoid commandline

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ else
 	ITERATION:=$(_ITERATION)
 endif
 
-all: docker
+all: hciscan
 
 build: $(BINARY)
 $(BINARY): main.go
@@ -24,38 +24,9 @@ $(BINARY).amd64.exe: main.go
 	# Building windows 64-bit x86 binary.
 	GOOS=windows GOARCH=amd64 go build -o $@ -ldflags "-w -s $(VERSION_LDFLAGS)"
 
-
 docker:
-	# docker buildx create --name hciscan
+	docker buildx create --name hciscan
 	docker buildx use hciscan
 	docker buildx inspect --bootstrap
 	docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/386 -t mabunixda/hciscan --push .
 
-docker.linux.amd64:
-	docker build -f Dockerfile \
-		--build-arg "BUILD_DATE=$(DATE)" \
-		--build-arg "COMMIT=$(COMMIT)" \
-		--build-arg "VERSION=$(VERSION)-$(ITERATION)" \
-		--build-arg "LICENSE=$(LICENSE)" \
-		--build-arg "DESC=$(DESC)" \
-		--build-arg "URL=$(URL)" \
-		--build-arg "VENDOR=$(VENDOR)" \
-		--build-arg "AUTHOR=$(MAINT)" \
-		--build-arg "BINARY=$(BINARY)" \
-		--build-arg "SOURCE_URL=$(SOURCE_URL)" \
-		--tag "mabunixda/$(BINARY):${VERSION}-amd64" .
-
-docker.linux.arm64:
-	docker build -f Dockerfile.arm64 \
-		--build-arg "BUILD_DATE=$(DATE)" \
-		--build-arg "COMMIT=$(COMMIT)" \
-		--build-arg "VERSION=$(VERSION)-$(ITERATION)" \
-		--build-arg "LICENSE=$(LICENSE)" \
-		--build-arg "DESC=$(DESC)" \
-		--build-arg "URL=$(URL)" \
-		--build-arg "VENDOR=$(VENDOR)" \
-		--build-arg "AUTHOR=$(MAINT)" \
-		--build-arg "BINARY=$(BINARY)" \
-		--build-arg "SOURCE_URL=$(SOURCE_URL)" \
-		--build-arg "ARCH=arm64" \
-		--tag "mabunixda/$(BINARY):${VERSION}-arm64" .
--- main.go
+++ main.go
@@ -1,51 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
+	"context"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"regexp"
+	"os/signal"
 	"time"
+
+	"github.com/go-ble/ble"
+	"github.com/go-ble/ble/examples/lib/dev"
+	"github.com/pkg/errors"
+)
+
+var (
+	device = flag.String("device", "default", "implementation of ble")
+	du     = flag.Duration("du", 5*time.Second, "scanning duration")
+	dup    = flag.Bool("dup", true, "allow duplicate reported")
 )
 
 var m map[string]time.Time
-
-func prepareHCI() {
-	prepareCmd := exec.Command("sh", "-c", "hciconfig hci0 down")
-	prepareCmd.Run()
-	prepareCmd = exec.Command("sh", "-c", "hciconfig hci0 up")
-	prepareCmd.Run()
-}
-
-func postMac(mac string) {
-	if _, ok := m[mac]; ok {
-		return
-	}
-	fmt.Println("detected mac: " + mac)
-	m[mac] = time.Now().UTC()
-	url := os.Getenv("ENDPOINT")
-	var jsonStr = []byte(`{ "mac": "` + mac + `" }`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == 200 {
-		return
-	}
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
-}
 
 func doEvery(d time.Duration, f func(time.Time)) {
 	for x := range time.Tick(d) {
@@ -53,7 +31,7 @@ func doEvery(d time.Duration, f func(time.Time)) {
 	}
 }
 
-func cleanUp(t time.Time) {
+func cacheCleanUp(t time.Time) {
 	if len(m) == 0 {
 		return
 	}
@@ -77,25 +55,92 @@ func main() {
 		fmt.Println("No endpoint env ariable set!")
 		return
 	}
-	prepareHCI()
+	flag.Parse()
+
 	m = make(map[string]time.Time)
-	go doEvery(15*time.Second, cleanUp)
-	re := regexp.MustCompile(`(([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2})`)
-	cmd := exec.Command("sh", "-c", "stdbuf -oL hcitool lescan --duplicates")
-	out, _ := cmd.StdoutPipe()
-	cmd.Start()
-	r := bufio.NewReader(out)
+	go doEvery(15*time.Second, cacheCleanUp)
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs)
+	// method invoked upon seeing signal
 	go func() {
-		err := cmd.Wait()
-		fmt.Println("Stopped - what? %v", err)
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		os.Exit(1)
 	}()
-	line, _, err := r.ReadLine()
-	fmt.Println(err)
-	for err == nil {
-		mac := re.FindString(string(line))
-		if mac != "" {
-			postMac(mac)
-		}
-		line, _, err = r.ReadLine()
+	d, err := dev.NewDevice(*device)
+	if err != nil {
+		log.Fatalf("can't new device : %s", err)
+	}
+	ble.SetDefaultDevice(d)
+	for {
+
+		// Scan for specified durantion, or until interrupted by user.
+		fmt.Printf("Scanning for %s...\n", *du)
+		ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), *du))
+		chkErr(ble.Scan(ctx, *dup, advHandler, nil))
+
+		time.Sleep(time.Second * time.Duration(2))
+
+	}
+}
+
+func advHandler(a ble.Advertisement) {
+	if a.Connectable() {
+		fmt.Printf("[%s] C %3d:", a.Addr(), a.RSSI())
+	} else {
+		fmt.Printf("[%s] N %3d:", a.Addr(), a.RSSI())
+	}
+	comma := ""
+	if len(a.LocalName()) > 0 {
+		fmt.Printf(" Name: %s", a.LocalName())
+		comma = ","
+	}
+	if len(a.Services()) > 0 {
+		fmt.Printf("%s Svcs: %v", comma, a.Services())
+		comma = ","
+	}
+	if len(a.ManufacturerData()) > 0 {
+		fmt.Printf("%s MD: %X", comma, a.ManufacturerData())
+	}
+	fmt.Printf("\n")
+	postMac(a.Addr().String())
+}
+
+func postMac(mac string) {
+	if _, ok := m[mac]; ok {
+		return
+	}
+	m[mac] = time.Now().UTC()
+	url := os.Getenv("ENDPOINT")
+	var jsonStr = []byte(`{ "mac": "` + mac + `" }`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		return
+	}
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
+
+func chkErr(err error) {
+	switch errors.Cause(err) {
+	case nil:
+	case context.DeadlineExceeded:
+		fmt.Printf("done\n")
+	case context.Canceled:
+		fmt.Printf("canceled\n")
+	default:
+		log.Fatalf(err.Error())
 	}
 }
